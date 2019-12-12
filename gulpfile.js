e17f00c796a0f5a3f27ee5cad5ef6225ce30684d
Jason Pamental <jason@rwt.io>
2019-12-12 16:40:22 -0500

    Getting build process working properly

--- gulpfile.js
+++ gulpfile.js
@@ -2,18 +2,20 @@
 
 
 // Initialize modules
-// Importing specific gulp API functions lets us write them below as series() instead of gulp.series()
-const { src, dest, watch, series, parallel } = require('gulp');
+const gulp = require('gulp');
 // Importing all the Gulp-related packages we want to use
 const concat = require('gulp-concat');
 const sourcemaps = require('gulp-sourcemaps');
 const sass = require('gulp-sass');
-const browserSync = require('browser-sync');
-const server = browserSync.create();
+//const browserSync = require('browser-sync');
+//const server = browserSync.create();
 const child = require('child_process');
 const gutil = require('gulp-util');
 const sassGlob = require('gulp-sass-glob');
 const webpack = require('webpack-stream');
+const del = require('del');
+
+const siteRoot = '_site';
 
 function reload(done) {
   server.reload();
@@ -22,55 +24,78 @@ function reload(done) {
 
 function serve(done) {
   server.init({
+    files: [siteRoot + '/**'],
+    port: 4000,
     server: {
-      baseDir: './'
+      baseDir: siteRoot
     }
   });
+  watchTask();
   done();
 }
 
 // File paths
 const files = { 
-    scssPath: '_includes/beatrice/styles.scss',
+    scssPath: '_includes/beatrice/**/*.scss',
     jsPath: '_includes/beatrice/**/*.js'
 }
 
 // Sass task: compiles the style.scss file into style.css
-function scssTask(done){    
-    return src(files.scssPath)
-        .pipe(sourcemaps.init()) // initialize sourcemaps first
-        .pipe(sassGlob())
-        .pipe(sass({
-          includePaths: ['node_modules']
-        }))
-        .pipe(sourcemaps.write('.')) // write sourcemaps file in current directory
-        .pipe(dest('assets/css')
-    ); // put final CSS in dist folder
-    done();
-  }
+function scssTask(){    
+  return gulp.src(files.scssPath)
+    .pipe(sourcemaps.init()) // initialize sourcemaps first
+    .pipe(sassGlob())
+    .pipe(sass({
+      includePaths: ['node_modules'],
+      outputStyle: 'compressed'
+    }))
+    .pipe(sourcemaps.write('.')) // write sourcemaps file in current directory
+    .pipe(gulp.dest('assets/css')); // put final CSS in dist folder
+}
 
 // JS task: compiles JS files into main.js
-function jsTask(done){    
-  return src(files.jsPath)
+function jsTask(){    
+  return gulp.src(files.jsPath)
       .pipe(webpack())
       .pipe(concat('main.js'))
-    .pipe(dest('assets/js')
-  ); // put final CSS in dist folder
-  done();
+      .pipe(gulp.dest('assets/js')); // put final JS in dist folder
 }
 
 // Watch task: watch SCSS and JS files for changes
 // If any change, run scss and js tasks simultaneously
 function watchTask(){
-    watch([files.scssPath], 
-        parallel(scssTask), reload);    
+  gulp.watch([files.scssPath, files.jsPath], gulp.series(scssTask, jsTask, jekyllBuild));    
+  gulp.watch(
+    [
+      './_layouts/**/*',
+      './_pages/**/*',
+      './_posts/**/*'
+    ], gulp.series(jekyllBuild))
 }
 
 function jekyll(done) {
   const jekyll = child.spawn('bundle', ['exec',
     'jekyll',
     'serve'
-  ]);
+]);
+
+  const jekyllLogger = (buffer) => {
+    buffer.toString()
+      .split(/\n/)
+      .forEach((message) => gutil.log('Jekyll: ' + message));
+  };
+
+  jekyll.stdout.on('data', jekyllLogger);
+  jekyll.stderr.on('data', jekyllLogger);
+  done();
+};
+
+function jekyllBuild(done) {
+  const jekyll = child.spawn('bundle', ['exec',
+    'jekyll',
+    'build'
+]);
+
 
   const jekyllLogger = (buffer) => {
     buffer.toString()
@@ -86,10 +111,9 @@ function jekyll(done) {
 // Export the default Gulp task so it can be run
 // Runs the scss and js tasks simultaneously
 // then runs cacheBust, then watch task
-exports.default = series(
-    parallel(scssTask), 
-    parallel(jsTask), 
-    //serve, 
+exports.default = gulp.series(
+    scssTask,
+    jsTask, 
     jekyll,
     watchTask
 );
--- index.markdown
+++ index.markdown
@@ -2,5 +2,29 @@
 
 layout: beatrice_page
 ---
-  <h1>Some stuff here</h1>
-  <p>And some text that goes with it</p>
+
+<h1>Some stuff here</h1>
+<p>And some text that goes with it</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>and some other stuff</p>
