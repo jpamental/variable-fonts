dd69542702bf26202e03fc0510afc38517e940ac
Jason Pamental <jason@rwt.io>
2019-12-12 17:20:27 -0500

    Starting to set up layout

--- _includes/beatrice/03-organisms/05-drawer/drawer.js
+++ _includes/beatrice/03-organisms/05-drawer/drawer.js
@@ -1,9 +1,12 @@
 import {MDCDrawer} from "@material/drawer";
-const drawer = MDCDrawer.attachTo(document.querySelector('.mdc-drawer'));
-
+if (document.querySelector('.mdc-drawer')) {
+  const drawer = MDCDrawer.attachTo(document.querySelector('.mdc-drawer'));
+}
 import {MDCTopAppBar} from "@material/top-app-bar";
-const topAppBar = MDCTopAppBar.attachTo(document.getElementById('app-bar'));
-topAppBar.setScrollTarget(document.getElementById('main-content'));
-topAppBar.listen('MDCTopAppBar:nav', () => {
-  drawer.open = !drawer.open;
-});
+if (document.querySelector('.mdc-drawer')) {
+  const topAppBar = MDCTopAppBar.attachTo(document.getElementById('app-bar'));
+  topAppBar.setScrollTarget(document.getElementById('main-content'));
+  topAppBar.listen('MDCTopAppBar:nav', () => {
+    drawer.open = !drawer.open;
+  });
+}
