54b5d27bb28914a74648e757679b7f3aa9a8d713
Jason Pamental <jason@rwt.io>
2019-12-13 16:27:15 -0500

    Update responsive navigation

--- _includes/beatrice/03-organisms/05-drawer/drawer.js
+++ _includes/beatrice/03-organisms/05-drawer/drawer.js
@@ -1,12 +1,9 @@
 import {MDCDrawer} from "@material/drawer";
-if (document.querySelector('.mdc-drawer')) {
   const drawer = MDCDrawer.attachTo(document.querySelector('.mdc-drawer'));
-}
+
 import {MDCTopAppBar} from "@material/top-app-bar";
-if (document.querySelector('.mdc-drawer')) {
   const topAppBar = MDCTopAppBar.attachTo(document.getElementById('app-bar'));
   topAppBar.setScrollTarget(document.getElementById('main-content'));
   topAppBar.listen('MDCTopAppBar:nav', () => {
     drawer.open = !drawer.open;
   });
-}
