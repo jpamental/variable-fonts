da15e529cdc47843ac08fe572d13e51cd480e5e8
kennethormandy <hello@kennethormandy.com>
2020-02-18 17:43:28 -0800

    Adds top app bar colour transition

--- _includes/beatrice/03-organisms/01-top-app-bar/top-app-bar.js
+++ _includes/beatrice/03-organisms/01-top-app-bar/top-app-bar.js
@@ -1,5 +1,23 @@
-import {MDCTopAppBar} from '@material/top-app-bar';
+import { MDCTopAppBar } from "@material/top-app-bar";
 
 // Instantiation
-const topAppBarElement = document.querySelector('.mdc-top-app-bar');
+const topAppBarElement = document.querySelector(".mdc-top-app-bar");
 const topAppBar = new MDCTopAppBar(topAppBarElement);
+
+const target = document.querySelector(".split-header");
+const className = "mdc-top-app-bar--blur";
+
+if (!!window.IntersectionObserver && target) {
+  let observer = new IntersectionObserver(
+    (entries, obs) => {
+      entries.forEach(entry => {
+        if (entry.intersectionRatio !== 1 && topAppBarElement.classList) {
+          topAppBarElement.classList.toggle(className);
+        }
+      });
+    },
+    { threshold: 0.1 }
+  );
+
+  observer.observe(target);
+}
