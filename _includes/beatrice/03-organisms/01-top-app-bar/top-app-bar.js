2cd13c474f3b4b8e178f2f71d20aff39e35d5d90
kennethormandy <hello@kennethormandy.com>
2020-02-20 17:43:46 -0800

    Adds explicit class to toggle top app bar background

--- _includes/beatrice/03-organisms/01-top-app-bar/top-app-bar.js
+++ _includes/beatrice/03-organisms/01-top-app-bar/top-app-bar.js
@@ -4,18 +4,16 @@ import { MDCTopAppBar } from "@material/top-app-bar";
 const topAppBarElement = document.querySelector(".mdc-top-app-bar");
 const topAppBar = new MDCTopAppBar(topAppBarElement);
 
-const target = document.querySelector(".split-header ");
+const target = document.querySelector(".js-top-app-bar--blur");
 const className = "mdc-top-app-bar--blur";
 
 if (!!window.IntersectionObserver && target) {
   let observer = new IntersectionObserver(
     (entries, obs) => {
       entries.forEach(entry => {
-        console.log("ratio", entry);
         if (topAppBarElement.classList && entry.isIntersecting) {
           topAppBarElement.classList.add(className);
         } else {
-          console.log('is not intersecting', entry)
           topAppBarElement.classList.remove(className);
         }
       });
