99734ff9fc71e96b7cde28feaab8480f473e8eac
kennethormandy <hello@kennethormandy.com>
2020-02-28 16:11:54 -0800

    Adds conditional for sliders without output (ie. in the hero animation)

--- _includes/beatrice/01-atoms/02-forms/slider/md-slider.js
+++ _includes/beatrice/01-atoms/02-forms/slider/md-slider.js
@@ -1,18 +1,28 @@
-import {MDCSlider} from '@material/slider';
+import { MDCSlider } from "@material/slider";
 
 //const slider = new MDCSlider(document.querySelector('.mdc-slider'));
 //slider.listen('MDCSlider:change', () => console.log(`Value changed to ${slider.value}`));
 
-var sliders = [].slice.call(document.querySelectorAll('.mdc-slider'));
+var sliders = [].slice.call(document.querySelectorAll(".mdc-slider"));
 
-sliders.forEach(function (slider) {
+sliders.forEach(function(slider) {
   const thisSlider = new MDCSlider(slider);
+  let el = document.getElementById(slider.id);
 
-  thisSlider.listen('MDCSlider:input', () => document.querySelector(':root').style.setProperty(`--${slider.id}`, thisSlider.value));
-  if (thisSlider.value < 1) {
-    thisSlider.listen('MDCSlider:input', () => document.getElementById(slider.id).parentElement.querySelector('.value-current').value=thisSlider.value.toFixed(2) );
-  } else {
-    thisSlider.listen('MDCSlider:input', () => document.getElementById(slider.id).parentElement.querySelector('.value-current').value=thisSlider.value );
+  if (el && el.parentElement) {
+    let output = document
+      .getElementById(slider.id)
+      .parentElement.querySelector(".value-current");
+
+    thisSlider.listen("MDCSlider:input", () =>
+      document
+        .querySelector(":root")
+        .style.setProperty(`--${slider.id}`, thisSlider.value)
+    );
+
+    thisSlider.listen("MDCSlider:input", () => {
+      output.value =
+        1 > thisSlider.value ? thisSlider.value.toFixed(2) : thisSlider.value;
+    });
   }
-
 });
